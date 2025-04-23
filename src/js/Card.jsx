bb660eb8efdcff9bba0201d4dfb7f85acd05d63b
Segundo Ferreira <segundo.ferreira28@gmail.com>
2025-04-23 17:59:15 +0000

    ultimos cambios

--- src/js/Card.jsx
+++ src/js/Card.jsx
@@ -1,30 +1,34 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-solid-svg-icons';
 
 export const Card = (props) => {
     return (
         <>
             <div className="counter">
                 <div className="cardnumbers1">
-                    <p>s</p>
+                    <p>
+                        <FontAwesomeIcon icon={faClock} />
+                    </p>
                 </div>
                 <div className="cardnumbers2">
-                    <p>{props.cardnumbers2  % 10}</p>
+                    <p>{props.cardnumbers2 % 10}</p>
                 </div>
                 <div className="cardnumbers3">
-                    <p>{props.cardnumbers3  % 10}</p>
+                    <p>{props.cardnumbers3 % 10}</p>
                 </div>
                 <div className="cardnumbers4">
-                    <p>{props.cardnumbers4  % 10 }</p>
+                    <p>{props.cardnumbers4 % 10}</p>
                 </div>
                 <div className="cardnumbers5">
                     <p>{props.cardnumbers5 % 10}</p>
                 </div>
                 <div className="cardnumbers6">
-                    <p>{props.cardnumbers6  % 10}</p>
+                    <p>{props.cardnumbers6 % 10}</p>
                 </div>
                 <div className="cardnumbers7">
-                    <p>{props.cardnumbers7  % 10}</p>
+                    <p>{props.cardnumbers7 % 10}</p>
                 </div>
             </div>
         </>
